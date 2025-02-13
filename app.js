1d0ad715c8cdaec5cdc2df73231f8eb27b0413a2
Md Tasmim Al Tahsin <tahsinniyan@gmail.com>
2025-02-13 17:56:22 +0600

    email template

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import { PORT } from './config/env.js';
 import userRouter from './routes/user.routes.js';
 import authRouter from './routes/auth.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
+import workflowRouter from './routes/workflow.routes.js';
 
 import connectToDatabase from './database/mongodb.js';
 
@@ -20,6 +21,7 @@ app.use(cookieParser());
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+app.use('/api/v1/workflows', workflowRouter);
 
 app.use(errorMiddleware);
 

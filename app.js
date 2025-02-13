d0dc19ac7712946fbb38a3a93dbfbe7f896a9322
Md Tasmim Al Tahsin <tahsinniyan@gmail.com>
2025-02-13 15:06:31 +0600

    controller added

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+
+import errorMiddleware from './middlewares/error.middleware.js';
 //import { PORT } from './config/env.js';
 const PORT=5500;
 
@@ -10,9 +13,15 @@ import connectToDatabase from './database/mongodb.js';
 
 const app = express();
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
 
 app.get('/', (req, res)=>{
     res.send("Welcome to subscription Tracker API");
